f9ff3114b0197422355c39de6aec8913f9b3c4c2
d776685-create <d776685@gmail.com>
2026-02-17 10:31:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,9 +29,6 @@ const MODEL_MAPPING = {
   'claude-3-opus': 'openai/gpt-oss-120b',
   'claude-3-sonnet': 'openai/gpt-oss-20b',
   'gemini-pro': 'qwen/qwen3-next-80b-a3b-thinking' 
-  'glm-5': 'z-ai/glm5'
-  'glm-4.7': 'z-ai/glm4.7'
-  'kimi' : 'moonshotai/kimi-k2.5'
   'deepseek' : 'deepseek-ai/deepseek-v3.2'
 };
 
